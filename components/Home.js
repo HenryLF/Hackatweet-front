164699ad42aab30ab19d07b325d24745536aec88
HenryLF <henrylf@protonmail.com>
2025-03-28 13:17:59 +0000

    style

--- components/Home.js
+++ components/Home.js
@@ -18,30 +18,40 @@ function Home() {
   return (
     <main className={styles.main}>
       <img src="./logoX.png" alt="logo du site" />
-      <h1>See what's happening</h1>
-      <h2> Join Hackatweet today. </h2>
-      <div className={styles.container}>
-        <Button
-          onClick={() => setSignUpVisible(!signUpVisible)}
-          className={styles.buttonSignUP}>
-          Sign Up
-        </Button>
-        <LoginForm
-          submitTo={(form) => dispatch(signUp(form))}
-          visible={signUpVisible}
-        />
-        <h3>Already have an account?</h3>
 
-        <Button
-          onClick={() => setSignInVisible(!signInVisible)}
-          className={styles.buttonSignIN}>
-          Sign In
-        </Button>
-        <LoginForm
-          submitTo={(form) => dispatch(signIn(form))}
-          visible={signInVisible}
-        />
-      </div>
+      {!signInVisible && !signUpVisible && (
+        <>
+          <h1>See what's happening</h1>
+          <h2> Join Hackatweet today. </h2>
+          <div className={styles.container}>
+            <Button
+              onClick={() => setSignUpVisible(true)}
+              className={styles.buttonSignUP}
+            >
+              Sign Up
+            </Button>
+            <h3>Already have an account?</h3>
+
+            <Button
+              onClick={() => setSignInVisible(true)}
+              className={styles.buttonSignIN}
+            >
+              Sign In
+            </Button>
+          </div>
+        </>
+      )}
+
+      <LoginForm
+        submitTo={(form) => dispatch(signUp(form))}
+        close={() => setSignUpVisible(false)}
+        visible={signUpVisible}
+      />
+      <LoginForm
+        submitTo={(form) => dispatch(signIn(form))}
+        close={() => setSignInVisible(false)}
+        visible={signInVisible}
+      />
     </main>
   );
 }
--- components/Trends.js
+++ components/Trends.js
@@ -2,27 +2,27 @@ import { useEffect, useState } from "react";
 import styles from "../styles/Trends.module.css";
 import { fetchTrends } from "../api/tweets";
 
-const TRENDS_UPDATE_DELAY = 20_000;
+const UPDATE_TRENDS_DELAY = 3_000; // 20 sec
 
 export default function Trends() {
-  const [trendsList, setTrendsList] = useState([]);
+  const [trendList, setTrendList] = useState([]);
 
   async function getTrends() {
     let jsonData = await fetchTrends();
-    jsonData.result && setTrendsList(jsonData.data);
+    jsonData.result && setTrendList(jsonData.data);
   }
-
   useEffect(() => {
-    let updateTrends = setInterval(getTrends, TRENDS_UPDATE_DELAY);
-    return () => clearInterval(updateTrends);
+    let updateTrends = setInterval(getTrends, UPDATE_TRENDS_DELAY);
+    return () => {
+      clearInterval(updateTrends);
+    };
   }, []);
-
-  console.log(trendsList);
-  const trends = trendsList.map((trend, id) => (
+  const trends = trendList.map((trend, id) => (
     <div key={id} className={styles.trend}>
       {trend.hashtag} {trend.count}
     </div>
   ));
+  console.log(trends);
 
   return <div className={styles.main}>{trends}</div>;
 }
